c65f6c4931352eb9fb8f8489f1fe745b9c14c659
Guilherme <guilherme.s.barros@outlook.com>
2025-08-24 20:38:20 -0300

    React Helmet (SEO)

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router'
 
 import { router } from './router'
-
 import './index.css'
 
 export function App() {
-	return <RouterProvider router={router} />
+	return (
+		<HelmetProvider>
+			<Helmet titleTemplate="%s | pizza.shop" />
+			<RouterProvider router={router} />
+		</HelmetProvider>
+	)
 }
