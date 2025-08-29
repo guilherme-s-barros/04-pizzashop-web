1917e14ddf7549a45f15af2e4acccfd9357694c1
Guilherme <guilherme.s.barros@outlook.com>
2025-08-29 19:54:12 -0300

    Componente: Detalhes do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,21 +1,28 @@
 import { ArrowRightIcon, SearchIcon, XIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
 	return (
 		<TableRow>
 			<TableCell>
-				<Button variant="outline" size="icon-sm">
-					<SearchIcon />
-					<span className="sr-only">Detalhes do pedido</span>
-				</Button>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button variant="outline" size="icon-sm">
+							<SearchIcon />
+							<span className="sr-only">Detalhes do pedido</span>
+						</Button>
+					</DialogTrigger>
+					<OrderDetails />
+				</Dialog>
 			</TableCell>
 			<TableCell className="font-mono text-xs font-medium">
 				neoroifjfjdsl
 			</TableCell>
-			<TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+			<TableCell className="text-muted-foreground">15 minutos atrás</TableCell>
 			<TableCell>
 				<span className="flex items-center gap-2 font-medium text-muted-foreground order-status-dot-pending">
 					Pendente
