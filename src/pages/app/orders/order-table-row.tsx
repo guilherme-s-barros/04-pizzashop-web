3bbacf9a80c558555bbabf359efedb354a32d667
Guilherme <guilherme.s.barros@outlook.com>
2025-09-05 20:07:00 -0300

    Operações no status do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -5,7 +5,10 @@ import { ArrowRightIcon, SearchIcon, XIcon } from 'lucide-react'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
+import { approveOrder } from '@/api/approve-order'
 import { cancelOrder } from '@/api/cancel-order'
+import { deliverOrder } from '@/api/deliver-order'
+import { dispatchOrder } from '@/api/dispatch-order'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
@@ -14,10 +17,12 @@ import { OrderStatus } from './order-status'
 
 import type { GetOrdersResponse } from '@/api/get-orders'
 
+type Status = 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+
 interface Order {
 	orderId: string
 	customerName: string
-	status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+	status: Status
 	total: number
 	createdAt: string
 }
@@ -30,38 +35,32 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
 	const [isDetailsOpen, setIsDetailsOpen] = useState(false)
 	const queryClient = useQueryClient()
 
-	const { mutateAsync: cancelOrderFn, isPending } = useMutation({
+	const { mutateAsync: cancelOrderFn, isPending: isCancelling } = useMutation({
 		mutationFn: cancelOrder,
 		onSuccess(_, { orderId }) {
-			queryClient.setQueriesData<GetOrdersResponse>(
-				{
-					queryKey: ['orders'],
-				},
-				(cacheData) => {
-					if (!cacheData) {
-						return
-					}
+			updateOrderStatusOnCache(orderId, 'canceled')
+		},
+	})
 
-					return {
-						...cacheData,
-						orders: cacheData.orders.map((order) => {
-							if (order.orderId === orderId) {
-								return {
-									...order,
-									status: 'canceled',
-								}
-							}
+	const { mutateAsync: approveOrderFn, isPending: isApproving } = useMutation({
+		mutationFn: approveOrder,
+		onSuccess(_, { orderId }) {
+			updateOrderStatusOnCache(orderId, 'processing')
+		},
+	})
 
-							return order
-						}),
-					}
-				},
-			)
+	const { mutateAsync: dispatchOrderFn, isPending: isDispatching } =
+		useMutation({
+			mutationFn: dispatchOrder,
+			onSuccess(_, { orderId }) {
+				updateOrderStatusOnCache(orderId, 'delivering')
+			},
+		})
 
-			queryClient.invalidateQueries({
-				queryKey: ['orders'],
-				stale: false,
-			})
+	const { mutateAsync: deliverOrderFn, isPending: isDelivering } = useMutation({
+		mutationFn: deliverOrder,
+		onSuccess(_, { orderId }) {
+			updateOrderStatusOnCache(orderId, 'delivered')
 		},
 	})
 
@@ -75,13 +74,82 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
 		currency: 'BRL',
 	})
 
+	function updateOrderStatusOnCache(orderId: string, status: Status) {
+		queryClient.setQueriesData<GetOrdersResponse>(
+			{
+				queryKey: ['orders'],
+			},
+			(cacheData) => {
+				if (!cacheData) {
+					return
+				}
+
+				return {
+					...cacheData,
+					orders: cacheData.orders.map((order) => {
+						if (order.orderId === orderId) {
+							return {
+								...order,
+								status,
+							}
+						}
+
+						return order
+					}),
+				}
+			},
+		)
+
+		queryClient.invalidateQueries({
+			queryKey: ['orders'],
+			stale: false,
+		})
+
+		queryClient.invalidateQueries({
+			queryKey: ['order_details'],
+			stale: false,
+		})
+	}
+
 	async function handleCancelOrder() {
 		try {
 			await cancelOrderFn({ orderId: order.orderId })
 
-			toast.success('Pedido cancelado com sucesso!')
+			toast.success('Pedido cancelado com sucesso.')
 		} catch {
-			toast.error('Erro ao cancelar pedido. Tente novamente mais tarde.')
+			toast.error('Não foi possível cancelar o pedido. Tente novamente.')
+		}
+	}
+
+	async function handleApproveOrder() {
+		try {
+			await approveOrderFn({ orderId: order.orderId })
+
+			toast.success('Pedido aceito e atualizado para "Em preparo".')
+		} catch {
+			toast.error('Não foi possível aceitar o pedido. Tente novamente.')
+		}
+	}
+
+	async function handleDispatchOrder() {
+		try {
+			await dispatchOrderFn({ orderId: order.orderId })
+
+			toast.success('Pedido atualizado para “Em entrega”.')
+		} catch {
+			toast.error(
+				'Não foi possível atualizar o status do pedido. Tente novamente.',
+			)
+		}
+	}
+
+	async function handleDeliverOrder() {
+		try {
+			await deliverOrderFn({ orderId: order.orderId })
+
+			toast.success('Pedido finalizado e atualizado para "Entregue".')
+		} catch {
+			toast.error('Não foi possível finalizar o pedido. Tente novamente.')
 		}
 	}
 
@@ -109,23 +177,54 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
 			<TableCell className="font-medium">{order.customerName}</TableCell>
 			<TableCell className="font-medium">{total}</TableCell>
 			<TableCell>
-				<Button variant="outline" size="sm">
-					<ArrowRightIcon />
-					Aprovar
-				</Button>
+				{order.status === 'pending' && (
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={handleApproveOrder}
+						disabled={isApproving}
+					>
+						<ArrowRightIcon />
+						Aprovar
+					</Button>
+				)}
+
+				{order.status === 'processing' && (
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={handleDispatchOrder}
+						disabled={isDispatching}
+					>
+						<ArrowRightIcon />
+						Em entrega
+					</Button>
+				)}
+
+				{order.status === 'delivering' && (
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={handleDeliverOrder}
+						disabled={isDelivering}
+					>
+						<ArrowRightIcon />
+						Entregue
+					</Button>
+				)}
 			</TableCell>
 			<TableCell>
-				<Button
-					variant="ghost"
-					size="sm"
-					disabled={
-						!['pending', 'processing'].includes(order.status) || isPending
-					}
-					onClick={handleCancelOrder}
-				>
-					<XIcon />
-					Cancelar
-				</Button>
+				{['pending', 'processing'].includes(order.status) && (
+					<Button
+						variant="ghost"
+						size="sm"
+						disabled={isCancelling}
+						onClick={handleCancelOrder}
+					>
+						<XIcon />
+						Cancelar
+					</Button>
+				)}
 			</TableCell>
 		</TableRow>
 	)
