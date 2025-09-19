f5231138bf39b68f70fb0d575ac8b56fbd7407d5
Guilherme <guilherme.s.barros@outlook.com>
2025-09-18 23:16:50 -0300

    Criando mocks de ações

---
+++ src/api/mocks/dispatch-order-mock.ts
@@ -0,0 +1,14 @@
+import { HttpResponse, http } from 'msw'
+
+import type { DispatchOrderParams } from '../dispatch-order'
+
+export const dispatchOrderMock = http.patch<DispatchOrderParams, never, never>(
+	'/orders/:orderId/dispatch',
+	({ params }) => {
+		if (params.orderId === 'error-order-id') {
+			return new HttpResponse(null, { status: 400 })
+		}
+
+		return new HttpResponse(null, { status: 204 })
+	},
+)
